778e4544f1b09417fedd2262442cbda83083b438
Mathis Girault <mathis.girault@grenoble-inp.org>
2022-10-24 16:12:21 +0200

    test

--- src/ensishell.c
+++ src/ensishell.c
@@ -134,8 +134,9 @@ void execute_command(struct cmdline *l) {
     }
 
     // if >
-    if (l->out != NULL) {    
-        int output_fd = open(l->out, O_WRONLY | O_CREAT);
+    if (l->out != NULL) {  
+        mode_t open_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+        int output_fd = open(l->out, O_RDWR | O_CREAT, open_mode);
         if (output_fd > -1) {
             dup2(output_fd, STDOUT_FILENO);
             close(output_fd);
