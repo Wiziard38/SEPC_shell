9593ec2801ea388493f0151e6885926fcb1ce9ba
Mathis Girault <giraulma@ensipc91.ensimag.fr>
2022-10-26 16:08:37 +0200

    add .h

---
+++ src/ensishell.h
@@ -0,0 +1,18 @@
+#ifndef __ENSISHELL_H
+#define __ENSISHELL_H
+
+#include "readcmd.h"
+
+typedef struct process {
+	pid_t process_id;
+	char *process_cmd;
+	struct process *next_process;
+} process;
+
+void add_bg_process(pid_t pid);
+
+void remove_bg_process(pid_t pid);
+
+void execute_command(struct cmdline *l);
+
+#endif
