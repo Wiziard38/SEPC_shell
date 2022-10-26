00094106f51c3ee01475902dd17774c12dae50b3
Mathis Girault <giraulma@ensipc91.ensimag.fr>
2022-10-26 16:09:11 +0200

    changement ordre pour pipe, changement printf to perror, changement affichage jobs

--- src/ensishell.c
+++ src/ensishell.c
@@ -12,7 +12,7 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <fcntl.h>
-
+#include "ensishell.h"
 
 #include "variante.h"
 #include "readcmd.h"
@@ -21,13 +21,10 @@
 #error "Variante non défini !!"
 #endif
 
-typedef struct process {
-	pid_t process_id;
-	char* process_cmd;
-	struct process *next_process;
-} process;
 
 struct process *bg_process_list = NULL;
+
+static char entered_command[100];
 
 /* Guile (1.8 and 2.0) is auto-detected by cmake */
 /* To disable Scheme interpreter (Guile support), comment the
@@ -45,10 +42,8 @@ int question6_executer(char *line)
 	 * parsecmd, then fork+execvp, for a single command.
 	 * pipe and i/o redirection are not required.
 	 */
-	printf("Not implemented yet: can not execute %s\n", line);
 
-	/* Remove this line when using parsecmd as it will free it */
-	free(line);
+	execute_command(parsecmd(&line));
 	
 	return 0;
 }
@@ -71,18 +66,21 @@ void terminate(char *line) {
 	exit(0);
 }
 
-void add_bg_process(pid_t pid, char* command) {
+void add_bg_process(pid_t pid) {
 	struct process *new_process = malloc(sizeof(struct process));
 	new_process->process_id = pid;
-	new_process->process_cmd = malloc(sizeof(command));
-	strcpy(new_process->process_cmd, command);
+	new_process->process_cmd = malloc(sizeof(entered_command));
+	strcpy(new_process->process_cmd, entered_command);
+	new_process->next_process = NULL;
 
 	if (bg_process_list == NULL) {
-		new_process->next_process = NULL;
 		bg_process_list = new_process;
 	} else {
-		new_process->next_process = bg_process_list;
-		bg_process_list = new_process;
+		struct process *current_process = bg_process_list;
+		while (current_process->next_process != NULL) {
+			current_process = current_process->next_process;	
+		}
+		current_process->next_process = new_process;
 	}
 }
 
@@ -90,7 +88,7 @@ void remove_bg_process(pid_t pid) {
 	process *current_process = bg_process_list;
 	process *previous_process = NULL;
 
-	if (current_process != NULL) {
+	while (current_process != NULL) {
 		if (current_process->process_id == pid) {
 			if (previous_process == NULL) {
 				bg_process_list = current_process->next_process;
@@ -102,111 +100,6 @@ void remove_bg_process(pid_t pid) {
 			return;
 		}
 	}
-}
-
-
-void execute_command(struct cmdline *l) {
-	pid_t pid;
-	int pipe_exists = false;
-	int pipefd[2];
-    int old_input_fd = dup(STDIN_FILENO);
-	int old_output_fd = dup(STDOUT_FILENO);
-	
-    // if |
-	if (l->seq[1] != NULL) {
-		pipe_exists = true;
-		if (pipe(pipefd) == -1) {
-			printf("Piping error! \n");
-			exit(0);
-		};
-	}
-
-    // if <
-    if (l->in != NULL) {
-        int input_fd = open(l->in, O_RDWR);
-        if (input_fd > -1) {
-            dup2(input_fd, STDIN_FILENO);
-            close(input_fd);
-        } else {
-            printf("Opening file error! \n");
-            exit(0);
-        }
-    }
-
-    // if >
-    if (l->out != NULL) {  
-        mode_t open_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-        int output_fd = open(l->out, O_RDWR | O_TRUNC | O_CREAT, open_mode);
-        if (output_fd > -1) {
-            dup2(output_fd, STDOUT_FILENO);
-            close(output_fd);
-        } else {
-            printf("Opening file error! \n");
-            exit(0);
-        }
-    }
-
-
-	if ((pid = fork()) == -1) {
-		printf("Forking error \n");
-	} else if (pid == 0) {
-
-		if (pipe_exists) {
-			dup2(pipefd[0], STDIN_FILENO);
-			if (close(pipefd[0]) == -1 || close(pipefd[1]) == -1 ) {
-				printf("Closing pipe error \n");
-				exit(0);
-			}
-            execvp(l->seq[1][0], l->seq[1]);
-		} else {
-            execvp(l->seq[0][0], l->seq[0]);
-        }
-
-		printf("Execvp error! \n" );
-		exit(0);
-	}
-
-    // if |
-	if (pipe_exists) {
-		if (fork() == 0) {
-			dup2(pipefd[1], STDOUT_FILENO);
-			if (close(pipefd[0]) == -1 || close(pipefd[1]) == -1 ) {
-				printf("Closing pipe error \n");
-				exit(0);
-			}
-
-			execvp(l->seq[0][0], l->seq[0]);
-			printf("Execvp error! \n" );
-			exit(0);
-		}
-	}
-
-    // if |
-    if (pipe_exists) {
-        if (close(pipefd[0]) == -1 || close(pipefd[1]) == -1 ) {
-            printf("Closing pipe error \n");
-            exit(0);
-		}
-    }
-
-    // if &
-	if(l->bg){
-		add_bg_process(pid, l->seq[0][0]);
-	} else {
-		int status = 0;
-		waitpid(pid, &status, 0);
-	}
-
-    if (l->in != NULL || pipe_exists == true) {
-        dup2(old_input_fd, STDIN_FILENO);
-    }
-
-    if (l->out != NULL || pipe_exists == true) {
-        dup2(old_output_fd, STDOUT_FILENO);
-    }
-
-    close(old_input_fd);	 		
-	close(old_output_fd);
 }
 
 
@@ -223,12 +116,13 @@ int main() {
 		struct cmdline *l;
 		char *line=0;
 		// int i, j;
-		char *prompt = "ensishell>";
+		char *prompt = "ensishell$ ";
 
 		/* Readline use some internal memory structure that
 		   can not be cleaned at the end of the program. Thus
 		   one memory leak per command seems unavoidable yet */
 		line = readline(prompt);
+		strcpy(entered_command, line);
 		if (line == 0 || ! strncmp(line,"exit", 4)) {
 			terminate(line);
 		}
@@ -258,48 +152,138 @@ int main() {
 			terminate(0);
 		}
 		
-
 		if (l->err) {
 			/* Syntax error, read another command */
 			printf("error: %s\n", l->err);
 			continue;
 		}
 
-		// if (l->in) printf("in: %s\n", l->in);
-		// if (l->out) printf("out: %s\n", l->out);
-		// if (l->bg) printf("background (&)\n");
-
-		// /* Display each command of the pipe */
-		// for (i=0; l->seq[i]!=0; i++) {
-		// 	char **cmd = l->seq[i];
-		// 	printf("seq[%d]: ", i);
-		// 	for (j=0; cmd[j]!=0; j++) {
-		// 			printf("'%s' ", cmd[j]);
-		// 	}
-		// 	printf("\n");
-		// }
-
-		if (strcmp(l->seq[0][0], "jobs") == 0) {
-			printf("========================= \n");
-			printf("Processes in background : \n");
+		if (*l->seq == NULL) {
+			// pass
+		} else if (strcmp(l->seq[0][0], "jobs") == 0) {
 			process *current_process = bg_process_list;
 			pid_t pid;
 			int child_state;
 
-			if (current_process != NULL) {
+			while (current_process != NULL) {
 				if (waitpid(current_process->process_id, &child_state, WNOHANG)) {
+					printf("[%d]   Done                        %s \n", current_process->process_id, current_process->process_cmd);
 					pid = current_process->process_id;
 					current_process = current_process->next_process;
-					remove_bg_process(pid); // replace pid with line ?
+					remove_bg_process(pid);
 				} else {
-					printf(" > Process %d : %s \n", current_process->process_id, current_process->process_cmd);
+					printf("[%d]   Running                     %s \n", current_process->process_id, current_process->process_cmd);
 					current_process = current_process->next_process;
 				}
-			}
-			printf("========================= \n");
-		
+			}		
 		} else {
 			execute_command(l);
 		}	
 	}
+}
+
+
+
+
+
+void execute_command(struct cmdline *l) {
+	pid_t pid;
+	int pipe_exists = false;
+	int pipefd[2];
+    int old_input_fd = dup(STDIN_FILENO);
+	int old_output_fd = dup(STDOUT_FILENO);
+	
+    // if |
+	if (l->seq[1] != NULL) {
+		pipe_exists = true;
+		if (pipe(pipefd) == -1) {
+			perror("Piping error");
+			exit(0);
+		};
+	}
+
+    // if <
+    if (l->in != NULL) {
+        int input_fd = open(l->in, O_RDWR);
+        if (input_fd > -1) {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        } else {
+            perror("Opening file error");
+            exit(0);
+        }
+    }
+
+    // if >
+    if (l->out != NULL) {  
+        mode_t open_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+        int output_fd = open(l->out, O_RDWR | O_TRUNC | O_CREAT, open_mode);
+        if (output_fd > -1) {
+            dup2(output_fd, STDOUT_FILENO);
+            close(output_fd);
+        } else {
+            perror("Opening file error");
+            exit(0);
+        }
+    }
+
+
+	if ((pid = fork()) == -1) {
+		perror("Forking error");
+	} else if (pid == 0) { // in child
+		if (pipe_exists) {
+			dup2(pipefd[0], STDIN_FILENO);
+			if (close(pipefd[1]) == -1 || close(pipefd[0]) == -1 ) {
+				perror("Closing pipe error");
+				exit(0);
+			}
+			execvp(l->seq[1][0], l->seq[1]);
+			perror("Execvp error" );
+			exit(0);
+		} else {
+			execvp(l->seq[0][0], l->seq[0]);
+			perror("Execvp error");
+			exit(0);
+		}
+	} else { // in parent
+		if (pipe_exists) {
+			if ((pid = fork()) == -1) {
+				perror("Forking error");
+			} else if (pid == 0) { // in second child
+				dup2(pipefd[1], STDOUT_FILENO);
+				if (close(pipefd[0]) == -1 || close(pipefd[1]) == -1 ) {
+					perror("Closing pipe error");
+					exit(0);
+				}
+
+				execvp(l->seq[0][0], l->seq[0]);
+				perror("Execvp error");
+				exit(0);
+			} else { // in second parent
+				if (close(pipefd[0]) == -1 || close(pipefd[1]) == -1 ) {
+					perror("Closing pipe error");
+					exit(0);
+				}
+			}
+		}
+
+		// wait for process to finish or add to background
+		if(l->bg){
+			add_bg_process(pid);
+		} else {
+			int status = 0;
+			waitpid(pid, &status, 0);
+		}
+	}
+
+	// Link back standard in and out
+    if (l->in != NULL || pipe_exists == true) {
+        dup2(old_input_fd, STDIN_FILENO);
+    }
+    if (l->out != NULL || pipe_exists == true) {
+        dup2(old_output_fd, STDOUT_FILENO);
+    }
+
+    close(old_input_fd);	 		
+	close(old_output_fd);
 }
